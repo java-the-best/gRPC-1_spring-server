52c6939b70a7599671cbd111a7ec5f0cf0bad6b1
java-the-best <84097429+java-the-best@users.noreply.github.com>
2021-08-22 21:46:07 +0300

    springBoot

---
+++ src/main/java/localhost/grpc1_springserver/Grpc1SpringServerApplication.java
@@ -0,0 +1,13 @@
+package localhost.grpc1_springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grpc1SpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grpc1SpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localhost/grpc1_springserver/Grpc1SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.grpc1_springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grpc1SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
